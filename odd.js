07a6a8041f2c75bc7c21b082bc32ff0545a457fa
Ali <alihbouran@gmail.com>
2022-03-18 09:57:02 -0700

    wrote function that finds if numbers are odd

---
+++ odd.js
@@ -0,0 +1,11 @@
+const isOdd = function (num) {
+  if (0 === num % 2) {
+  return false
+  } else {
+    return true
+  }
+  
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
